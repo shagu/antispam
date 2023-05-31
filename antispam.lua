7a7116ae8400e507493c6e465e072a63fd78165e
shagu <ericmauser@googlemail.com>
2023-05-31 14:17:28 +0200

    remove all special characters from messages

--- antispam.lua
+++ antispam.lua
@@ -1,3 +1,6 @@
+local debug = false
+local msg
+
 local events = {
   ["CHAT_MSG_CHANNEL"] = true,
   ["CHAT_MSG_YELL"] = true,
@@ -12,19 +15,32 @@ local blocks = {
   { "mooo" },
 }
 
+local prepare = function(msg)
+  msg = string.gsub(msg, "[^A-Za-z0-9]", "")
+  return msg
+end
+
+local debug = function(msg)
+  if not debug then return end
+  DEFAULT_CHAT_FRAME:AddMessage("|cff33ffccBLOCKED|r"..msg)
+end
+
 local _ChatFrame_OnEvent = ChatFrame_OnEvent
 function ChatFrame_OnEvent(event)
   if events[event] and arg2 and arg1 then
+    msg = prepare(arg1)
+
     for _, data in pairs(blocks) do
       local matched = true
 
       for _, str in pairs(data) do
-        if not strfind(string.lower(arg1), string.lower(str)) then
+        if not strfind(string.lower(msg), string.lower(str)) then
           matched = false
         end
       end
 
       if matched == true then
+        debug(msg)
         return true
       end
     end
